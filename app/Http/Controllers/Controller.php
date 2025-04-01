aac55f568c8ae3c377d6dcdf85a2edaca6e6962a
HenriqueGF1 <goncalves.f.henrique@gmail.com>
2025-04-01 19:50:23 -0300

    Add swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,15 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *     title="Minhas Leituras Documentação",
+ *     version="1.0.0",
+ *     description="Gerenciamento de Leituras Este projeto é uma aplicação de gerenciamento de leituras onde os usuários podem cadastrar, organizar e acompanhar seu progresso de leitura de livros, HQs e outros materiais. O sistema permite que os usuários gerenciem suas leituras, definam metas, escrevam avaliações, recebam lembretes para ler, e muito mais."
+ * )
+ */
+
+
 abstract class Controller
 {
     //
